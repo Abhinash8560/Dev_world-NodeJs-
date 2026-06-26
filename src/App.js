7531ded5169bd220cfcf78f4784d63c5c2c6559e
Abhinash <abhinash@Abhinashs-MacBook-Air.local>
2026-06-26 16:08:29 +0530

    envSetup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+console.log("JWT_SECRET =", process.env.JWT_SECRET);
 const express = require('express');
 const connectDB=require('./config/database');
 const app = express();
@@ -8,9 +10,16 @@ const authRouter=require('./Routes/auth');
 const profileRouter=require('./Routes/profile');
 const requestRouter=require('./Routes/request');
 const userRouter=require('./Routes/user');
-
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,39 +1,33 @@
-// const adminAuth=(req,res,next)=>{
-//     console.log("Admin Auth is getting checked");
-//     const token='xyz';
-//     const isAdminAuthorized = token === 'xyz';
-//     if(!isAdminAuthorized){
-//         res.status(401).send("Unauthorized: Admin access required")
-//     } else {
-//         next();
-//     }
-// };
+
 
 const jwt=require('jsonwebtoken');
 const User=require('../models/User');
 
 const UserAuth=async (req,res,next)=>{
   //read the token from the req cookie
-try{ 
-     const {token}=req.cookies;
-     if(!token){
-        // return res.status(401).send("Unauthorized: No token provided");
-         }
-  const decodedObject=await jwt.verify(token,"DEV@COM$790");
-  const {_id}=decodedObject;
-  const user=await User.findById(_id);
-  if(!user){
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).send("Please Login!");
+    }
+
+    
+      const decodedObj = await jwt.verify(token, process.env.JWT_SECRET);
+  const { _id } = decodedObj;
+
+  const user = await User.findById(_id);
+
+  if (!user) {
     return res.status(404).send("User not found");
   }
-  req.user=user;
+
+  req.user = user;
   next();
-} catch(err){
-  console.log(err);
-  res.status(400).send("Error:Token is invalid!!!!" );
+} catch (err) {
+  console.error(err);
+  res.status(401).send("Invalid token");
 }
-  //validate the token
-  //find the User
-};
+}
 module.exports = {
     // adminAuth,
     UserAuth
